72754a41f1ca2e18f78f53e4d2fc5eeca791f340
9B7C <16whitijarl@washk12.org>
2014-01-30 14:27:56 -0700

    Sky coloring
    Sky coloring added

--- source/draw.py
+++ source/draw.py
@@ -1,7 +1,49 @@
 import pygame
 import graphics
 import game
+import random
 from sprite import Sprite
+for i in range(100):
+    random.seed(random.randint(1,1000000000))
+
+colorList=[
+    13, #random.randint(0,255),
+    13, #random.randint(0,255),
+    random.randint(0,255)
+]
+
+colorChangeList=[0,0,0]
+
+def solveColor(listPosition):
+    # Add to the color value using the current color change rate
+    colorList[listPosition]+=colorChangeList[listPosition]
+
+    # Make sure it is in range
+    if colorList[listPosition]>255:
+        colorList[listPosition]=255
+    elif colorList[listPosition]<0:
+        colorList[listPosition]=0
+
+    # Add to the color change rate
+    colorChangeList[listPosition]+=random.randint(-1,1)
+
+    # Make sure it is in range
+    if colorChangeList[listPosition]>5:
+        colorChangeList[listPosition]=5
+    elif colorChangeList[listPosition]<-5:
+        colorChangeList[listPosition]=-5
+
+    # return the color
+    return colorList[listPosition]
+
+def solveColors():
+    #r=solveColor(0)
+    r=13
+    #g=solveColor(1)
+    g=13
+    b=solveColor(2)
+    return (r,g,b)
+
 
 def draw():
     """
@@ -9,7 +51,7 @@ def draw():
     """
 
     # Draw background
-    game.screen.fill((18, 161, 251))
+    game.screen.fill(solveColors())
     
     # Drawing a sprite called my_sprite
     # my_sprite.draw()
