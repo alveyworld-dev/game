7e88f81b4b7dbd427fd68bdc003835c7341111bf
Joshua Beitler <joshbeitler@gmail.com>
2014-02-03 10:04:52 -0700

    Fixed slowness

--- source/sprite.py
+++ source/sprite.py
@@ -3,7 +3,8 @@ import game
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, texture, position):
-        self.image 	= pygame.image.load(game.rpath + texture)
+        self.image 	= pygame.image.load(game.rpath + texture).convert()
+        self.image.set_colorkey(0)
         self.rect 	= self.image.get_rect()
      	self.rect.x = position[0]
      	self.rect.y = position[1]
