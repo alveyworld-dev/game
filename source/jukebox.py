de1d00831e2e872f4e2d02da752233da4e243044
Joshua Beitler <joshbeitler@gmail.com>
2014-02-03 10:37:14 -0700

    Jukebox

---
+++ source/jukebox.py
@@ -0,0 +1,11 @@
+from source.sound import Sound, Music
+from random import randint
+
+class Jukebox:
+    """
+    This here class is for the random playing of background music.  Basically,
+    we will pick a random music track from the avalible ones, and play them
+    randomly at game start
+    """
+    
+    pass
